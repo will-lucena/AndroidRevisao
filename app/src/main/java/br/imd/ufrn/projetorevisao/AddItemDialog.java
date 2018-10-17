ed38670c4f5712e85609498218463c9cf3f58dfd
William Lucena Dos Santos <williamaster@imd.local>
2018-10-17 19:46:33 -0300

    Trabalhando na acao dos itens da lista

---
+++ app/src/main/java/br/imd/ufrn/projetorevisao/AddItemDialog.java
@@ -0,0 +1,58 @@
+package br.imd.ufrn.projetorevisao;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddItemDialog extends DialogFragment {
+
+    private EditText editText;
+    private OnTextListener listener;
+
+    public static void show(FragmentManager fm, OnTextListener listener){
+
+        AddItemDialog dialog = new AddItemDialog();
+        dialog.listener = listener;
+        dialog.show(fm, "addItemDialog");
+
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Item");
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(listener != null){
+                    String text = editText.getText().toString();
+                    listener.onSetTExt(text);
+                }
+            }
+        });
+
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dismiss();
+            }
+        });
+
+        View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_add_item,null);
+        editText = view.findViewById(R.id.itemName);
+        builder.setView(view);
+        return builder.create();
+    }
+
+    public interface OnTextListener{
+        void onSetTExt(String text);
+    }
+}
--- app/src/main/java/br/imd/ufrn/projetorevisao/MainActivity.java
+++ app/src/main/java/br/imd/ufrn/projetorevisao/MainActivity.java
@@ -2,12 +2,73 @@ package br.imd.ufrn.projetorevisao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String> adapter;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ListView list = findViewById(R.id.itemsList);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        list.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                AddItemDialog.show(getSupportFragmentManager(), new AddItemDialog.OnTextListener() {
+                    @Override
+                    public void onSetTExt(String text) {
+                        setText(text);
+                    }
+                });
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    public void setText(String content)
+    {
+        adapter.add(content);
+        /*
+        TextView textView = findViewById(R.id.textView);
+
+        textView.setText(content);
+        /**/
     }
 }
