c381c8663de38e2f5858c2ab29905efbcdb73653
Will <will.lucen4@gmail.com>
2018-10-17 01:04:35 -0300

    Finalizado criacao do botao na actionBar, trabalhando na comunicacao da dialog de addItem

--- app/src/main/java/br/imd/ufrn/projetorevisao/ActionBarActivity.java
+++ app/src/main/java/br/imd/ufrn/projetorevisao/ActionBarActivity.java
@@ -1,10 +1,18 @@
 package br.imd.ufrn.projetorevisao;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActionBarActivity extends AppCompatActivity {
 
@@ -17,22 +25,48 @@ public class ActionBarActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_settings:
-                // User chose the "Settings" item, show the app settings UI...
-                return true;
+            case R.id.add:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Item");
+                builder.setView(R.layout.fragment_add_item_dialog);
 
-            case R.id.action_favorite:
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
-                return true;
+// Set up the buttons
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //EditText input = findViewById(R.id.itemName);
+                        //Toast.makeText(null, "ok", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
 
+                builder.show();
+                return true;
             default:
                 // If we got here, the user's action was not recognized.
                 // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    public void setText(String content)
+    {
+        TextView textView = findViewById(R.id.textView);
+
+        textView.setText(content);
     }
 }
