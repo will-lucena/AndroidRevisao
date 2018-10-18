758e760d06e0850db5f9672852ac962df0918973
Will <will.lucen4@gmail.com>
2018-10-18 01:04:12 -0300

    App finalizado, problemas pra unselect itens

--- app/src/main/java/br/imd/ufrn/projetorevisao/AddItemDialog.java
+++ app/src/main/java/br/imd/ufrn/projetorevisao/AddItemDialog.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
--- app/src/main/java/br/imd/ufrn/projetorevisao/MainActivity.java
+++ app/src/main/java/br/imd/ufrn/projetorevisao/MainActivity.java
@@ -1,21 +1,27 @@
 package br.imd.ufrn.projetorevisao;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArrayAdapter<String> adapter;
     private Toolbar toolbar;
+    private ListView list;
+    private String selectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,29 +30,54 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ListView list = findViewById(R.id.itemsList);
+        list = findViewById(R.id.itemsList);
+
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = adapter.getItem(position);
-                Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
+                selectedItem = adapter.getItem(position);
+                view.setSelected(true);
+                hideAddButton();
             }
         });
-
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         list.setAdapter(adapter);
     }
 
+    private void showAddButton()
+    {
+        toolbar.getMenu().findItem(R.id.actionsAdd).setVisible(true);
+        toolbar.getMenu().findItem(R.id.actionsEdit).setVisible(false);
+        toolbar.getMenu().findItem(R.id.actionsRemove).setVisible(false);
+    }
+
+    private void hideAddButton()
+    {
+        toolbar.getMenu().findItem(R.id.actionsAdd).setVisible(false);
+        toolbar.getMenu().findItem(R.id.actionsEdit).setVisible(true);
+        toolbar.getMenu().findItem(R.id.actionsRemove).setVisible(true);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.action, menu);
+        getMenuInflater().inflate(R.menu.actions, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.add:
+            case R.id.actionsAdd:
+                AddItemDialog.show(getSupportFragmentManager(), new AddItemDialog.OnTextListener() {
+                    @Override
+                    public void onSetTExt(String text) {
+                        setText(text);
+                    }
+                });
+                showAddButton();
+                return true;
+            case R.id.actionsEdit:
+                //Toast.makeText(getApplicationContext(), list.getSelectedItemPosition(), Toast.LENGTH_SHORT).show();
                 AddItemDialog.show(getSupportFragmentManager(), new AddItemDialog.OnTextListener() {
                     @Override
                     public void onSetTExt(String text) {
@@ -54,21 +85,28 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
                 return true;
+            case R.id.actionsRemove:
+                adapter.remove(selectedItem);
+                showAddButton();
+                return true;
             default:
-                // If we got here, the user's action was not recognized.
+                // If we got here, the user's actions was not recognized.
                 // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
-
         }
     }
 
     public void setText(String content)
     {
-        adapter.add(content);
-        /*
-        TextView textView = findViewById(R.id.textView);
-
-        textView.setText(content);
-        /**/
+        int itemPosition = adapter.getPosition(selectedItem);
+        if (itemPosition >= 0)
+        {
+            adapter.insert(content, itemPosition);
+            adapter.remove(selectedItem);
+        }
+        else
+        {
+            adapter.add(content);
+        }
     }
 }
